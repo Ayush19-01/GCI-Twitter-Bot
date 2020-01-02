07e406cd6da55ea0067243b180becf298ff39b64
Ayush19-01 <57313104+Ayush19-01@users.noreply.github.com>
2020-01-02 09:36:46 +0530

    Update tweeter.py

--- tweeter.py
+++ tweeter.py
@@ -9,13 +9,14 @@ link=a["entries"][0]["links"][0]["href"]
 toadd="Have a read on this latest fedora magazine article on\n"+title+"\nClick to read the article:"+link
 a=input("Enter twitter username:")
 b=input("Enter twitter password:")
-driver = webdriver.Chrome()
+driver = webdriver.Chrome("./chromedriver")
 driver.get("https://twitter.com/login")
 username = driver.find_element_by_css_selector("input[placeholder='Phone, email or username']")
 password= driver.find_element_by_css_selector("input[class='js-password-field']")
 username.send_keys(a)
 password.send_keys(b)
-submit = driver.find_element_by_xpath("//button[text()='Log in']")
+time.sleep(3)
+submit = driver.find_element_by_xpath('//*[@id="page-container"]/div/div[1]/form/div[2]/button')
 submit.click()
 time.sleep(7)
 for i in range(14):
